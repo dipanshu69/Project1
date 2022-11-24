8e100defb74f11be196452b9a1e6abeecffb9b33
Dipanshu <deepanshu.patil69@gmail.com>
2022-11-24 12:06:16 +0530

    home header footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-;
+import React from "react";
+import "../styles/footer.scss";
+
+
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer>
+      <div>
+        <h1>ESSENET TECHNOLOGIES</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+        <a href="" target={"blank"}>
+          Youtube
+        </a>
+        <a href="" target={"blank"}>
+          Facebook
+        </a>
+        <a href="" target={"blank"}>
+          Instagram
+        </a>
+        <a href="https://github.com/dipanshu69" target={"blank"}>
+          GitHub
+        </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
